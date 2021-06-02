bfa9fa23014dbe2884df3a151215a25d1baa1487
luo <1973653885@qq.com>
2021-06-02 15:48:58 +0800

    pcl_visualizerer_demo_fun1

--- pcl_visualizer_demo/pcl_visualizer_demo.cpp
+++ pcl_visualizer_demo/pcl_visualizer_demo.cpp
@@ -46,6 +46,7 @@ pcl::visualization::PCLVisualizer::Ptr viewportsVis(pcl::PointCloud<pcl::PointXY
 	// --------------------------------------------------------
 	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
 	viewer->initCameraParameters();
+
 	int v1(0);
 	viewer->createViewPort(0.0, 0.0, 0.5, 1.0, v1);
 	viewer->setBackgroundColor(0, 0, 0, v1);
@@ -61,11 +62,145 @@ pcl::visualization::PCLVisualizer::Ptr viewportsVis(pcl::PointCloud<pcl::PointXY
 	viewer->addPointCloud<pcl::PointXYZRGB>(cloud, single_color, "sample cloud2", v2);
 
 	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud1");
-	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "normals2", v2);
+	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud2");
 	viewer->addCoordinateSystem(1.0);
 
 	viewer->addPointCloudNormals<pcl::PointXYZRGB, pcl::Normal>(cloud, normals1, 10, 0.05, "normals1", v1);
 	viewer->addPointCloudNormals<pcl::PointXYZRGB, pcl::Normal>(cloud, normals2, 10, 0.05, "normals2", v2);
 
 	return (viewer);
+}
+
+// ----------------
+// -----主函数-----
+// ----------------
+int 
+main(int argc, char** argv)
+{
+	// --------------------------------------
+	// ----------命令行参数解析--------------
+	// --------------------------------------
+	if (pcl::console::find_argument(argc, argv, "-h") >= 0)
+	{
+		printUsage(argv[0]);
+		return 0;
+	}
+	bool simple(false), rgb(false), custom_c(false), normals(false),
+		shapes(false), viewports(false), interaction_customization(false);
+	if (pcl::console::find_argument(argc, argv, "-s") >= 0)
+	{
+		simple = true;
+		std::cout << "Simple visualisation example\n";
+	}
+	else if (pcl::console::find_argument(argc, argv, "-c") >= 0)
+	{
+		custom_c = true;
+		std::cout << "Custom colour visualisation example\n";
+	}
+	else if (pcl::console::find_argument(argc, argv, "-r") >= 0)
+	{
+		rgb = true;
+		std::cout << "RGB colour visualisation example\n";
+	}
+	else if (pcl::console::find_argument(argc, argv, "-n") >= 0)
+	{
+		normals = true;
+		std::cout << "Normals visualisation example\n";
+	}
+	else if (pcl::console::find_argument(argc, argv, "-a") >= 0)
+	{
+		shapes = true;
+		std::cout << "Shapes visualisation example\n";
+	}
+	else if (pcl::console::find_argument(argc, argv, "-v") >= 0)
+	{
+		viewports = true;
+		std::cout << "Viewports example\n";
+	}
+	else if (pcl::console::find_argument(argc, argv, "-i") >= 0)
+	{
+		interaction_customization = true;
+		std::cout << "Interaction Customization example\n";
+	}
+	else
+	{
+		printUsage(argv[0]);
+		return 0;
+	}
+	// ------------------------------------
+	// ----------创建示例点云数据----------
+	// ------------------------------------
+	pcl::PointCloud < pcl::PointXYZ > ::Ptr basic_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr(new pcl::PointCloud<pcl::_PointXYZRGB>);
+	std::cout << "Generating example point clouds.\n\n";
+	/*
+	 * 创建一个向z轴挤压的椭圆，颜色由按红绿蓝的顺序渐变
+	 */
+	std::uint8_t r(255), g(15), b(15);
+	for (float z(-1.0); z <= 1.0; z += 0.05)
+	{
+		for (float angle(0.0); angle <= 360.0; angle += 5.0)
+		{
+			pcl::PointXYZ basic_point;
+			basic_point.x = 0.5*std::cos(pcl::deg2rad(angle));
+			basic_point.y = sinf(pcl::deg2rad(angle));
+			basic_point.z = z;
+			basic_cloud_ptr->points.push_back(basic_point);
+
+			pcl::PointXYZRGB point;
+			point.x = basic_point.x;
+			point.y = basic_point.y;
+			point.z = basic_point.z;
+			std::uint32_t rgb = (static_cast<std::uint32_t>(r) << 16 | static_cast<std::uint32_t>(g) << 8
+				| static_cast<std::uint32_t>(b));
+			//将rgb的数据保存在一个浮点数
+			point.rgb = *reinterpret_cast<float*> (&rgb);
+			point_cloud_ptr->points.push_back(point);
+		}
+		if (z < 0.0)
+		{
+			r -= 12;
+			g += 12;
+		}
+		else
+		{
+			g -= 12;
+			b += 12;
+		}
+	}
+	basic_cloud_ptr->width = basic_cloud_ptr->size();
+	basic_cloud_ptr->height = 1;//无序点云
+	point_cloud_ptr->width = point_cloud_ptr->size();
+	point_cloud_ptr->height = 1;
+
+	// ----------------------------------------------------------------
+	// ----------------以搜索半径0.05计算表面法线----------------------
+	// ----------------------------------------------------------------
+	pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
+	ne.setInputCloud(point_cloud_ptr);
+	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>());
+	ne.setSearchMethod(tree);
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals1(new pcl::PointCloud<pcl::Normal>);
+	ne.setRadiusSearch(0.05);
+	ne.compute(*cloud_normals1);
+	// ----------------------------------------------------------------
+	// ----------------以搜索半径0.1计算表面法线----------------------
+	// ----------------------------------------------------------------
+	pcl::PointCloud<pcl::Normal>::Ptr cloud_normals2(new pcl::PointCloud<pcl::Normal>);
+	ne.setRadiusSearch(0.1);
+	ne.compute(*cloud_normals2);
+
+	pcl::visualization::PCLVisualizer::Ptr viewer;
+	if (viewports)
+	{
+		viewer = viewportsVis(point_cloud_ptr, cloud_normals1, cloud_normals2);
+	}
+	//--------------------
+	// -----Main loop-----
+	//--------------------
+	while (!viewer->wasStopped())
+	{
+		viewer->spinOnce(100);
+		std::this_thread::sleep_for(100ms);
+	}
 }
