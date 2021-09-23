826397a4465a96e4a52da81ad7a4526438f069e5
luo <1973653885@qq.com>
2021-09-23 14:53:56 +0800

    NDT done

--- pcl_registration/4_normal_distributions_transform/source/normal_distributions_transform.cpp
+++ pcl_registration/4_normal_distributions_transform/source/normal_distributions_transform.cpp
@@ -8,30 +8,39 @@
 int
 main (int argc, char** argv)
 {
+	/*
+		加载数据
+	*/
   //加载房间的第一次扫描
   pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud (new pcl::PointCloud<pcl::PointXYZ>);
   if (pcl::io::loadPCDFile<pcl::PointXYZ> ("room_scan1.pcd", *target_cloud) == -1)
   {
-    PCL_ERROR ("Couldn't read file room_scan1.pcd \n");
-    return (-1);
+	PCL_ERROR ("Couldn't read file room_scan1.pcd \n");
+	return (-1);
   }
   std::cout << "Loaded " << target_cloud->size () << " data points from room_scan1.pcd" << std::endl;
   //加载从新视角得到的房间的第二次扫描
   pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud (new pcl::PointCloud<pcl::PointXYZ>);
   if (pcl::io::loadPCDFile<pcl::PointXYZ> ("room_scan2.pcd", *input_cloud) == -1)
   {
-    PCL_ERROR ("Couldn't read file room_scan2.pcd \n");
-    return (-1);
+	PCL_ERROR ("Couldn't read file room_scan2.pcd \n");
+	return (-1);
   }
   std::cout << "Loaded " << input_cloud->size () << " data points from room_scan2.pcd" << std::endl;
-  //将输入的扫描过滤到原始尺寸的大概10%以提高匹配的速度。
+
+  /*
+	滤波
+  */
+  //将输入的扫描过滤到原始尺寸的大概 10% 以提高匹配的速度。
   pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud (new pcl::PointCloud<pcl::PointXYZ>);
   pcl::ApproximateVoxelGrid<pcl::PointXYZ> approximate_voxel_filter;
   approximate_voxel_filter.setLeafSize (0.2, 0.2, 0.2);
   approximate_voxel_filter.setInputCloud (input_cloud);
   approximate_voxel_filter.filter (*filtered_cloud);
   std::cout << "Filtered cloud contains " << filtered_cloud->size ()
-            << " data points from room_scan2.pcd" << std::endl;
+			<< " data points from room_scan2.pcd" << std::endl;
+
+
   //初始化正态分布变换（NDT）
   pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt;
   //设置依赖尺度NDT参数
@@ -42,20 +51,23 @@ main (int argc, char** argv)
   //设置NDT网格结构的分辨率（VoxelGridCovariance）
   ndt.setResolution (1.0);
   //设置匹配迭代的最大次数
-  ndt.setMaximumIterations (35);
+  ndt.setMaximumIterations (50);
   // 设置要配准的点云
   ndt.setInputCloud (filtered_cloud);
   //设置点云配准目标
   ndt.setInputTarget (target_cloud);
+
   //设置使用机器人测距法得到的初始对准估计结果
   Eigen::AngleAxisf init_rotation (0.6931, Eigen::Vector3f::UnitZ ());
   Eigen::Translation3f init_translation (1.79387, 0.720047, 0);
   Eigen::Matrix4f init_guess = (init_translation * init_rotation).matrix ();
   //计算需要的刚体变换以便将输入的点云匹配到目标点云
   pcl::PointCloud<pcl::PointXYZ>::Ptr output_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  ndt.align (*output_cloud, init_guess);
+  //ndt.align (*output_cloud, init_guess);
+  ndt.align (*output_cloud);
   std::cout << "Normal Distributions Transform has converged:" << ndt.hasConverged ()
-            << " score: " << ndt.getFitnessScore () << std::endl;
+			<< " score(分数越大，配准效果越差): " << ndt.getFitnessScore () << std::endl;
+
   //使用创建的变换对未过滤的输入点云进行变换
   pcl::transformPointCloud (*input_cloud, *output_cloud, ndt.getFinalTransformation ());
   //保存转换的输入点云
@@ -69,21 +81,21 @@ main (int argc, char** argv)
   target_color (target_cloud, 255, 0, 0);
   viewer_final->addPointCloud<pcl::PointXYZ> (target_cloud, target_color, "target cloud");
   viewer_final->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE,
-                                                  1, "target cloud");
+												  1, "target cloud");
   //对转换后的目标点云着色（绿色）并可视化
   pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
   output_color (output_cloud, 0, 255, 0);
   viewer_final->addPointCloud<pcl::PointXYZ> (output_cloud, output_color, "output cloud");
   viewer_final->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE,
-                                                  1, "output cloud");
+												  1, "output cloud");
   // 启动可视化
   viewer_final->addCoordinateSystem (1.0);
   viewer_final->initCameraParameters ();
   //等待直到可视化窗口关闭。
   while (!viewer_final->wasStopped ())
   {
-    viewer_final->spinOnce (100);
-    boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+	viewer_final->spinOnce (100);
+	boost::this_thread::sleep (boost::posix_time::microseconds (100000));
   }
   return (0);
 }
